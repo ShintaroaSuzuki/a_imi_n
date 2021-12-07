5fcf87bec53adf9a73c1f85c3138e8f163f0a504
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2021-12-08 03:25:12 +0900

    [add] メニューボタンにCSSアニメーションを追加

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -1,10 +1,13 @@
-import styles from '../styles/MenuButton.module.css' 
+import styles from '../styles/components/MenuButton.module.scss' 
+import { useState } from 'react'
 
 const MenuButton = () => {
+  const [clicked, setClicked] = useState<Boolean>(false);
+
   return (
-    <div className={styles.menuContainer}>
-      <div className={styles.menuBar}/>
-      <div className={styles.menuBar}/>
+    <div className={styles.menuContainer} onClick={() => setClicked(!clicked)}>
+      <div className={clicked ? styles['menuBar-clicked'] : styles['menuBar'] } />
+      <div className={clicked ? styles['menuBar-clicked'] : styles['menuBar'] } />
     </div>
   )
 }
