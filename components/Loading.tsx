b66fc5d77b5fdbc4f1f94e62c4e1b6367d5a88b2
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2021-12-12 22:57:38 +0900

    [modify] スマホで閲覧したときにvhが機能しない問題を修正

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,7 +1,30 @@
 import Loader from 'react-loader-spinner'
 import styles from '../styles/components/Loading.module.scss'
+import { useEffect } from 'react'
 
 const Loading = () => {
+  useEffect(() => {
+      const setFillHeight = () => {
+        const vh = window.innerHeight * 0.01
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+      }
+
+      const setFillWidth = () => {
+        const vw = window.innerWidth * 0.01
+        document.documentElement.style.setProperty('--vw', `${vw}px`)
+      }
+
+      const handleResize = () => {
+        setFillHeight();
+        setFillWidth();
+      }
+
+      window.addEventListener('resize', handleResize);
+
+      handleResize()
+    }, []
+  )
+
   return (
     <div className={styles["loading-container"]}>
       <Loader
