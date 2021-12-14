d2d4829eb5af9559025e03dcc6020bcc42cfd228
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2021-12-15 02:19:22 +0900

    [modify] react-uniconsでビルドエラーが発生しないように修正

--- components/MailForm.tsx
+++ components/MailForm.tsx
@@ -1,5 +1,5 @@
 import styles from '../styles/components/MailForm.module.scss'
-import { UilTwitter, UilInstagram, UilEnvelope } from '@iconscout/react-unicons'
+const { UilTwitter, UilInstagram, UilEnvelope } = require('@iconscout/react-unicons')
 
 const MailForm = ({ loading }: { loading: Boolean }) => {
   return (
@@ -9,15 +9,15 @@ const MailForm = ({ loading }: { loading: Boolean }) => {
       </div>
       <div className={styles["text-box"]}>
         <UilTwitter size="20" color="gray"/>
-        <text className={styles["text"]}>Twitter: @a_imi_n</text>
+        <text className={styles["text"]}>@a_imi_n</text>
       </div>
       <div className={styles["text-box"]}>
         <UilInstagram size="20" color="gray"/>
-        <text className={styles["text"]}>Instagram: @a_imi_n</text>
+        <text className={styles["text"]}>@a_imi_n</text>
       </div>
       <div className={styles["text-box"]}>
         <UilEnvelope size="20" color="gray"/>
-        <text className={styles["text"]}>Mail: a.imi.n.t.0508@gmail.com</text>
+        <text className={styles["text"]}>a.imi.n.t.0508@gmail.com</text>
       </div>
     </div>
   )
