e5ef9f3f3ad5c06896e9f291eab03dfc9b4750d5
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2021-12-20 23:51:40 +0900

    [add] MenuButtonに引数を追加

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -1,8 +1,13 @@
 import styles from '../styles/components/MenuButton.module.scss' 
 import { useState } from 'react'
 
-const MenuButton = ({loading}: {loading: Boolean}) => {
+const MenuButton = ({loading, setMenuOpened}: {loading: Boolean, setMenuOpened: (menuOpend: Boolean) => void}) => {
   const [clicked, setClicked] = useState<Boolean>(false);
+
+  const _onClick = () => {
+    setClicked(!clicked)
+    setMenuOpened(true)
+  }
 
   return (
     <div className={loading ? styles["menuContainer-loaded"] : styles["menuContainer"]} onClick={() => setClicked(!clicked)}>
